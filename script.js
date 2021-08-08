332563de1cfccabd67420c1eaa0b692333b8240d
muditm006 <88556020+muditm006@users.noreply.github.com>
2021-08-08 00:07:11 -0400

    Added uniform text+image on the google maps markers.

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function initMap()
 			({
 				position: props.coords,
 				map:map,
-				icon: 'park.png'
+				icon: 'images/park.png'
 			});
 
 			/*
@@ -78,34 +78,35 @@ function initMap()
 		}
 
 	// Listen for click on map
-
+	
+	/*
 	google.maps.event.addListener(map, 'click',
 	function(event)
 	{
 		addMarker({coords:event.latLng, iconImage: 'park.png'});
 	}
 	);
+	*/
 
 
 	// Creates array of markers
 
 	var markers= [{coords:{lat: 25.6785, lng: -80.27422},
-	    content:"<h1>Fairchild Tropical Botanical Garden</h1><h3>Described by many as a tropical wonderland, Fairchild is home to a beautiful selection of rare, tropical plants and native flowers. It also focuses on educating its visitors, as it offers school programs, scholarships, and even graduate fellowships.</h3><img src='fairchild.jpg' class='image1' alt='Scenery at Fairchild Garden'>"},
+		content: "<center><h1>Fairchild Tropical Botanical Garden</h1></center><div class='container'><div class='image'> <img style='width:300px;height:200px;' src='images/fairchild.jpg' alt='Scenery at Fairchild Garden'></div><div class='text'><h3>Described by many as a tropical wonderland, Fairchild is home to a beautiful selection of rare, tropical plants and native flowers. It also focuses on educating its visitors, as it offers school programs, scholarships, and even graduate fellowships.</h3></div></div>"},
 	{coords:{lat: 25.849405269180505, lng: -80.16459259788341},
-	    content:"<h1>Pelican Harbor Seabird Station</h1><h3>Since its founding, the Pelican Harbor Seabird Station has done priceless work, rescuing over 35,821 native birds, mammals, and reptiles. Visitors are given the opportunity to volunteer to care for the animals being taken care of by cleaning after and feeding them. The organization also provides Seabird Cruises, which take passengers along waters to observe over 30 species of birds.</h3><img src='pelican_harbor.png' alt='Releasing a pelican at Pelican Harbor'>"},
+		content: "<center><h1>Pelican Harbor Seabird Station</h1></center><div class='container'><div class='image'> <img style='width:300px;height:200px;' img src='images/pelican_harbor.png' alt='Releasing a pelican at Pelican Harbor'></div><div class='text'><h3>Since its founding, the Pelican Harbor Seabird Station has done priceless work, rescuing over 35,821 native birds, mammals, and reptiles. Visitors are given the opportunity to volunteer to care for the animals being taken care of by cleaning after and feeding them. The organization also provides Seabird Cruises, which take passengers along waters to observe over 30 species of birds.</h3></div></div>"},
 	{coords:{lat: 26.499573832858726, lng: -80.21232937408512},
-	    content:"<h1>Arthur R. Marshall Loxahatchee National Wildlife Refuge</h1><h3>This wildlife sanctuary houses the most northern remnant of the historic Everglades wetlands, protecting the integrity of the remaining ecosystem. Visitor activities include hiking, biking, wildlife observation, photography, and much more!</h3><img src='loxahatchee.jpg' alt='Sunset at Loxahatchee Wildlife Refuge'>"},
-	    //this image is the only one that's a bit small
+		content: "<center><h1>Arthur R. Marshall Loxahatchee National Wildlife Refuge</h1></center><div class='container'><div class='image'> <img style='width:300px;height:200px;' img src='images/loxahatchee.jpg' alt='Sunset at Loxahatchee Wildlife Refuge'></div><div class='text'><h3>This wildlife sanctuary houses the most northern remnant of the historic Everglades wetlands, protecting the integrity of the remaining ecosystem. Visitor activities include hiking, biking, wildlife observation, photography, and much more!</h3></div></div>"},
 	{coords:{lat: 26.36631133640211, lng: -80.07013981422405},
-	    content:"<h1>Gumbo Limbo Nature Center</h1><h3>Gumbo Limbo is a fun, interactive nature center focused on rehabilitating marine wildlife, specifically sea turtles. Visitors can adopt a sea turtle, go on a walk on their Boardwalk Trail, check out their Butterfly Garden, and so much more. They are also often involved in beach cleanups!</h3><img src='gumbo-limbo.jpg' alt='Sea Turtle at Gumbo Limbo Nature Center'>"},
+		content: "<center><h1>Gumbo Limbo Nature Center</h1></center><div class='container'><div class='image'> <img style='width:300px;height:200px;' src='images/gumbo-limbo.jpg' alt='Sea Turtle at Gumbo Limbo Nature Center'></div><div class='text'><h3>Gumbo Limbo is a fun, interactive nature center focused on rehabilitating marine wildlife, specifically sea turtles. Visitors can adopt a sea turtle, go on a walk on their Boardwalk Trail, check out their Butterfly Garden, and so much more. They are also often involved in beach cleanups!</h3></div></div>"},
 	{coords:{lat: 29.167783709963093, lng: -81.79159879100125},
-	    content:"<h1>Ocala National Forest</h1><h3>Ocala National Forest is known for its large areas of sand pine scrub forest. Its multiple springs and recreation areas have clean, natural pools and canoe runs, which have been cited to be very relaxing and fun!</h3><img src='ocala.jpg' alt='A spring in Ocala National Forest'>"},
+	    content: "<center><h1>Ocala National Forest</h1></center><div class='container'><div class='image'> <img style='width:300px;height:200px;' src='images/ocala.jpg' alt='A spring in Ocala National Forest'></div><div class='text'><h3>Ocala National Forest is known for its large areas of sand pine scrub forest. Its multiple springs and recreation areas have clean, natural pools and canoe runs, which have been cited to be very relaxing and fun!</h3></div></div>"},
 	{coords:{lat: 25.484541535607914, lng: -80.20791265433957},
-	    content:"<h1>Biscayne National Park</h1><h3>This park allows visitors to take a break from the busy streets of downtown Miami for relaxation in nature. It works to conserve and protect a unique blend of aquamarine waters, emerald islands, and fish-bejeweled coral reefs. Visitors are able to boat, snorkel, camp, watch wildlife, or just take a walk to see the stunning views.</h3><img src='biscayne.jpg' alt='Lighthouse at Biscayne National Park'>"},
+		content: "<center><h1>Biscayne National Park</h1></center><div class='container'><div class='image'> <img style='width:300px;height:200px;' src='images/biscayne.jpg' alt='Lighthouse at Biscayne National Park'></div><div class='text'><h3>This park allows visitors to take a break from the busy streets of downtown Miami for relaxation in nature. It works to conserve and protect a unique blend of aquamarine waters, emerald islands, and fish-bejeweled coral reefs. Visitors are able to boat, snorkel, camp, watch wildlife, or just take a walk to see the stunning views.</h3></div></div>"},
 	    //this image's width fits perfectly, but height too big sigh
 	{coords:{lat: 25.27471779658046, lng: -80.90248989934324},
-	    content:"<h1>Everglades National Park </h1><h3>The Everglades National Park protects the southern 20% of the original Everglades in Florida, encompassing 1.5 million acres of tropical and subtropical habitat with one of the world’s most diverse ecosystems. Visitors are able to interact with wildlife; tour the park on boat, bike, or foot; and immerse themselves in the wonders of the swamp.</h3><img src='everglades.jpg' alt='Sunset in the Everglades'>"}];
-
+		content: "<center><h1>Everglades National Park </h1></center><div class='container'><div class='image'> <img style='width:300px;height:200px;' src='images/everglades.jpg' alt='Sunset in the Everglades'></div><div class='text'><h3>The Everglades National Park protects the southern 20% of the original Everglades in Florida, encompassing 1.5 million acres of tropical and subtropical habitat with one of the world’s most diverse ecosystems. Visitors are able to interact with wildlife; tour the park on boat, bike, or foot; and immerse themselves in the wonders of the swamp.</h3></div></div>"}];
+	    
 	// Loops through array of markers, adding them to map
 
 	for(var i=0; i< markers.length; i++)
